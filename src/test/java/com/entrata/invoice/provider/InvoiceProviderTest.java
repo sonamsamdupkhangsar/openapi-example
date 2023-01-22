cb88636aa476a5511827b290848180c2d51eb027
ssamdupkhangsar <ssamdupkhangsar@entrata.com>
2023-01-22 12:24:53 -0700

    provider test runs now but with error

--- src/test/java/com/entrata/invoice/provider/InvoiceProviderTest.java
+++ src/test/java/com/entrata/invoice/provider/InvoiceProviderTest.java
@@ -1,22 +1,11 @@
 package com.entrata.invoice.provider;
-/*
-import au.com.dius.pact.provider.PactVerification;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit5.PactVerificationContext;
-import au.com.dius.pact.provider.junit5.TestTarget;*/
 
-/*
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import au.com.dius.pact.provider.spring.junit5.WebFluxTarget;
-
-import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import com.entrata.invoice.InvoiceHandler;
 import com.entrata.invoice.Router;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,10 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-*/
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,8 +34,7 @@ public class InvoiceProviderTest {
     @BeforeEach
     public void setup(PactVerificationContext context) {
         LOG.info("setup running..");
-        //context.setTarget(new WebFluxTarget(router.route(invoiceHandler)));
-        //context.setTarget(new WebFluxTest(routert);
+        context.setTarget(new WebFluxTarget(router.route(invoiceHandler)));
     }
 
     @TestTemplate
